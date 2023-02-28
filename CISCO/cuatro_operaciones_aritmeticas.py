775984f356169b1752d3894da1d7105d74b03ec9
SEBASTIANRODRIGUE <johans_rodriguez12@soy.sena.edu.co>
2023-02-28 11:45:21 -0500

    cisco

---
+++ CISCO/cuatro_operaciones_aritmeticas.py
@@ -0,0 +1,9 @@
+#La tarea es completar el código para evaluar y mostrar el resultado de cuatro operaciones aritméticas básicas.
+a = float(input('primer numero:'))
+b = float(input('segundo numero:'))
+print('suma' + str(a+b))
+print('resta' + str(a-b))
+print('multiplicacion' + str(a*b))
+print('Division' + str(a/b))
+
+print("\n¡Eso es todo, amigos!")
---
+++ CISCO/el_cuadrado_funcion_float_input
@@ -0,0 +1,3 @@
+a = float(input("Inserta un número: "))
+b = a ** 2.0
+print(a, "al cuadrado es", b)
