30295422bccf450cd7c2777e4f7bad1fa13f8cf5
SEBASTIANRODRIGUE <johans_rodriguez12@soy.sena.edu.co>
2023-02-28 11:32:55 -0500

    cisco

---
+++ CISCO/4_operaciones_aritmeticas.py
@@ -0,0 +1,9 @@
+float(input("Ingrese el primer número:"'num1='))
+float(input("Ingrese el primer número:"'num2='))
+
+
+print("La suma de", num1, "y", num2, "es igual a:", suma)
+print("La resta de", num1, "y", num2, "es igual a:", resta)
+print("La multiplicación de", num1, "y", num2, "es igual a:", multiplicacion)
+print("La división de", num1, "y", num2, "es igual a:", division)
+print("\n¡Eso es todo, amigos!")
