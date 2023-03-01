6a49e766ee8b711b4d9e3818e84bd186cae5ab42
SEBASTIANRODRIGUE <johans_rodriguez12@soy.sena.edu.co>
2023-03-01 11:27:14 -0500

    cisco

---
+++ CISCO/operador_igualdad.py/desigualdad_(!=).py
@@ -0,0 +1,7 @@
+#El operador != (no es igual a) también compara los valores de dos operandos. Aquí está la diferencia: si son iguales, el resultado de la comparación es False. Si no son iguales, el resultado de la comparación es True.
+
+var = 0 
+print(var != 0) #imrprime False
+
+var = 1 
+print(var != 0) #imprime true
