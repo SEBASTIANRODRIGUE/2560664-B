38c8145ea9af90ce4bcfd09cd3a047e5a621d864
johan sebastian rodriguez <johans_rodriguez12@soy.sena.edu.co>
2023-03-06 17:34:40 -0500

    quiz

---
+++ quiz/main.py
@@ -0,0 +1,10 @@
+from profesor import profesor
+
+from estudiante import estudiante
+
+#ingrese un profesor
+
+nombre_profesor = input('ingrese el nompre del profesor:')
+edad_profesor = init(input('ingrese la edad del profesor:'))
+asignatura_profesor=input('ingrese las asignaturas que enseña el profesor (separadas por comas):'). split(",")
+profesor = profesor
---
+++ quiz/profesor.py
@@ -0,0 +1,17 @@
+
+#modulo del profesor para ingresar al sistema 
+
+class profesor: 
+    def __init__(self, nombre, edad, asignaturas):
+        self.nombre = nombre
+        self.edad= edad
+        self. asignaturas = asignaturas
+
+        def enseñar (self):
+            print (f'{self.nombre} esta enseñando {len (self.asignaturas)} asignaturas')
+
+        def enseñar (self):
+            print(f'{self.nombre} esta enseñando{len(self.asignaturas)} asignaturas')
+
+        def consultar (self):
+            print(f'{self.nombre} esta disponible para consultas')        
